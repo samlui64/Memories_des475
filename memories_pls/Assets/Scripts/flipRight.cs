1118ed80e6c5d1e29c729c9fd55065b25b3164c4
jhoangmuzic <60382844+jhoangmuzic@users.noreply.github.com>
2020-11-17 22:49:36 -0800

    flipRight

---
+++ memories_pls/Assets/Scripts/flipRight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipRight : MonoBehaviour
+{
+    Vector3 characterScale;
+    float characterScaleX;
+
+    void Start()
+    {
+        characterScale = transform.localScale;
+        characterScaleX = characterScale.x;
+    }
+
+    void Update () {
+
+        // Move the Character:
+        transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
+
+        // Flip the Character:
+        if (Input.GetAxis("Horizontal") < 0) {
+            characterScale.x = characterScaleX;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = -characterScaleX;
+        }
+        transform.localScale = characterScale;
+}
+}
