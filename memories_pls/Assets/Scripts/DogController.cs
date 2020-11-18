0f381bfc2e9c3dadec49d6cbe9b95411dbd4fef1
SamL <saml@users.noreply.github.com>
2020-11-17 19:40:13 -0800

    Import Assets, Add controller
    Imported stand-in background. Imported final dog player. Added player controller (player can move/control the dog).

---
+++ memories_pls/Assets/Scripts/DogController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.02f * horizontal;
+        position.y = position.y + 0.02f * vertical;
+        transform.position = position;
+    }
+
+}
